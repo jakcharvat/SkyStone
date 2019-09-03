6a0892e057ad94fdc401d57082e46bdcc7c87404
jakcharvat <jakcharvat@gmail.com>
2019-09-03 16:45:58 +0300

    Create Move.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Move.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Move {
+
+    private DcMotor leftMotor, rightMotor;
+
+    public Move(HardwareMap hardwareMap) {
+        this.leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        this.rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+    }
+
+    void moveForward() {
+        // Do something
+    }
+
+    void moveBackwards() {
+        // Do something else 
+    }
+
+}
