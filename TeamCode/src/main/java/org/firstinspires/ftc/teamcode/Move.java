a6def6271117b28c67b8c6834dafb45409082398
jakcharvat <jakcharvat@gmail.com>
2019-09-11 16:54:39 +0300

    Fixed motor direction, replaced wait() with Thread.sleep() and fixed wrong motor assignment on Move.setPowerOn()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Move.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Move.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
@@ -22,6 +23,9 @@ public class Move {
         this.rightFrontMotor = hardwareMap.dcMotor.get("rightFrontMotor");
         this.leftBackMotor = hardwareMap.dcMotor.get("leftBackMotor");
         this.rightBackMotor = hardwareMap.dcMotor.get("rightBackMotor");
+
+        this.leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.leftBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     /**
@@ -52,7 +56,7 @@ public class Move {
     void straight(final double power, final int forMilliseconds) throws InterruptedException {
         straight(power);
 
-        wait(forMilliseconds);
+        Thread.sleep(forMilliseconds);
 
         stop();
     }
@@ -82,7 +86,7 @@ public class Move {
         this.leftFrontMotor.setPower(left);
         this.leftBackMotor.setPower(left);
         this.rightFrontMotor.setPower(right);
-        this.leftBackMotor.setPower(right);
+        this.rightBackMotor.setPower(right);
     }
 
 }
