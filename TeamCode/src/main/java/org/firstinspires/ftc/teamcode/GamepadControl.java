5771c99884f60d0cf3adf9b90a58240b11d7b323
Jakub Charvat <jakcharvat@gmail.com>
2019-10-29 17:02:23 +0300

    Update GamepadControl.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadControl.java
@@ -17,7 +17,7 @@ public class GamepadControl extends LinearOpMode {
         while (opModeIsActive()) {
 
             if (teleMove.getRobotSetup().baseType() == BaseType.diagonal) {
-                teleMove.diagonalBaseGamepadMove(gamepad1.left_stick_y < 0 ? gamepad1.right_stick_x : -gamepad1.right_stick_x, gamepad1.left_stick_y, gamepad1.left_stick_x, telemetry);
+                teleMove.diagonalBaseGamepadMove(gamepad1.left_stick_y < 0 ? gamepad1.left_stick_x : -gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, telemetry);
             } else {
                 teleMove.gamepadTurn(gamepad1.right_stick_x, gamepad1.right_stick_y);
             }
