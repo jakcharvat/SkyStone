5f66fca1d282e17af9ab0a85902eb66d652e48f4
Jakub Charvat <jakcharvat@gmail.com>
2019-11-26 14:25:42 +0300

    Remove duplicate classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotSetup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotSetup.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
